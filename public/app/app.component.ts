19eb232822e02cfaa995f9961d890954d053b1ae
RJFreund <robert.joseph.freund@gmail.com>
2016-06-30 11:50:29 -0500

    converting app to angular2

---
+++ public/app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by RJ-LAPTOP on 6/30/2016.
+ */
+import { Component } from '@angular/core';
+@Component({
+	selector: 'my-app',
+	template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
