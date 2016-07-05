23a4a9470e65646cf462008c0c8c67aab4f0fc1e
RJFreund <robert.joseph.freund@gmail.com>
2016-07-05 11:03:55 -0500

    slowly adding authentication code

--- public/shared/app.js
+++ public/shared/app.js
@@ -1,6 +1,23 @@
-var tasksApp = angular.module('tasks', ['ui.router', 'ui.router.title', 'ngSanitize', 'ngResource', 'ui.bootstrap']);
-tasksApp.config(function($stateProvider, $urlRouterProvider){
+var app = angular.module('tasks', ['ui.router', 'ui.router.title', 'ngSanitize', 'ngResource', 'ui.bootstrap']);
+app.config(function($stateProvider, $urlRouterProvider){
 	$urlRouterProvider.otherwise("/")
 	$stateProvider
-		.state("home", {});
+		.state("home", {})
+		.state("login", {});
 });
+
+app.factory("Session", function(){
+
+});
+
+app.run(['$rootScope', '$location', '$state', 'constructionService', '$anchorScroll',
+    function($rootScope, $location, $state, constructionService, $anchorScroll){
+        $rootScope.$on('$stateChangeStart',
+            function(event, toState, toParams, fromState, fromParams){
+            	//make sure that the user is logged in
+            	/* if (!isUserLoggedIn) {
+					redirectToLoginPage();
+            	}
+				*/
+            });        
+}]);
